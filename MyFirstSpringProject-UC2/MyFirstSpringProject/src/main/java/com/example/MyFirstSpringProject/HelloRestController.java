4ba5891d7d09f73f9d8e971f15e5382fc6bee5a1
Aarushi2146 <aarug2146@gmail.com>
2025-03-01 15:14:24 +0530

    request param annotation

---
+++ MyFirstSpringProject-UC2/MyFirstSpringProject/src/main/java/com/example/MyFirstSpringProject/HelloRestController.java
@@ -0,0 +1,26 @@
+package com.example.MyFirstSpringProject;
+
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+public class HelloRestController {
+
+    //http://localhost:8080/
+    @RequestMapping("/")
+    public String index(){
+        return "hello from bridgeLabz!";
+    }
+
+
+    //localhost:8080/hello/query?name=Aarushi
+    @GetMapping("/hello/query")
+    public String sayHello(@RequestParam(value = "name") String name){
+        return "Hello "+ name +"from bridgeLabz!";
+    }
+
+
+//    @GetMapping("/hello/param/{name}")
+//    public String sayHello1(@PathVariable String name) {
+//        return "Hello " + name + " from BridgeLabz!";
+//    }
+}
